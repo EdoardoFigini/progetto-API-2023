fcf75032ea5e717361ace3a4ee5a40a325f60e9f
Edoardo Figini <edoardoeffe4@gmail.com>
2023-09-03 23:16:58 +0200

    edit

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #define MAX_COMMAND 18
 #define NIL         -1
+#define RED         0
 #define WHITE       0
 #define BLACK       1
 #define GRAY        2
@@ -25,10 +26,12 @@ struct bst_node{
     struct bst_node* next;
     unsigned int steps;
     unsigned short int color;
+    unsigned short int rb;
 };
 
 typedef struct{
     struct bst_node* root;
+    struct bst_node* nil;
     size_t size;
 } bst_t;
 
@@ -39,38 +42,190 @@ typedef struct queue {
     size_t length;
 } queue_t;
 
-
-// static void debug_tree(bst_t*);
-
 static bst_t* init_tree(){
     bst_t* t;
 
     t=(bst_t*)malloc(sizeof(bst_t));
-    t->root = NULL;
+    t->nil = malloc(sizeof(struct bst_node));
+    t->root = t->nil;
+    t->nil->parent = t->root;
+    t->nil->key = NIL;
+    memset(t->nil->cars.a, 0, sizeof(int) * MAX_CARS);
+    t->nil->cars.size = 0;
+    t->nil->left = NULL;
+    t->nil->right = NULL;
+    t->nil->rb = BLACK;
+    
 
     return t;
 }
 
-static struct bst_node* tree_minimum(struct bst_node* x){
-    while (x->left != NULL)
+static void left_rotate(bst_t* t, struct bst_node* x){
+    struct bst_node* y;
+
+
+    y = x->right;
+    x->right = y->left;
+    if(y->left != t->nil)
+        y->left->parent = x;
+    y->parent = x->parent;
+    if(x->parent == t->nil)
+        t->root = y;
+    else if(x == x->parent->left)
+        x->parent->left = y;
+    else
+        x->parent->right = y;
+    y->left = x;
+    x->parent = y;
+}
+
+static void right_rotate(bst_t* t, struct bst_node* x){
+    struct bst_node* y;
+
+    
+    y = x->left;
+    x->left = y->right;
+    if(y->right != t->nil)
+        y->right->parent = x;
+    y->parent = x->parent;
+    if(x->parent == t->nil)
+        t->root = y;
+    else if(x == x->parent->left)
+        x->parent->left = y;
+    else
+        x->parent->right = y;
+    y->right = x;
+    x->parent = y;
+}
+
+static struct bst_node* tree_minimum(bst_t* t, struct bst_node* x){
+    while (x->left != t->nil)
         x = x->left;
 
     return x;
 }
 
-static struct bst_node* tree_successor(struct bst_node* x){
+static struct bst_node* tree_successor(bst_t* t, struct bst_node* x){
     struct bst_node* y;
 
-    if(x->right != NULL)
-        return tree_minimum(x->right);
+    if(x->right != t->nil)
+        return tree_minimum(t, x->right);
     y = x->parent;
-    while(y!=NULL && x==y->right){
+    while(y!=t->nil && x==y->right){
         x = y;
         y = y->parent;
     }
 
     return y;
 }
+
+static int rb_insert_fixup(bst_t* t, struct bst_node* z){
+    struct bst_node *x;
+    struct bst_node *y;
+
+
+    if(z == t->root)
+        t->root->rb = BLACK;
+    else{
+        x = z->parent;
+        if(x->rb == RED){
+            if(x == x->parent->left){
+                y = x->parent->right;
+                if(y->rb == RED){
+                    x->rb = BLACK;
+                    y->rb = BLACK;
+                    x->parent->rb = RED;
+                    rb_insert_fixup(t, x->parent);
+                } else {
+                    if(z == x->right) {
+                        z = x;
+                        left_rotate(t, z);
+                        x = z->parent;
+                    }
+                    x->rb = BLACK;
+                    x->parent->rb = RED;
+                    right_rotate(t, x->parent);
+                }
+            } else {
+                y = x->parent->left;
+                if(y->rb == RED){
+                    x->rb = BLACK;
+                    y->rb = BLACK;
+                    x->parent->rb = RED;
+                    rb_insert_fixup(t, x->parent);
+                } else {
+                    if(z == x->left) {
+                        z = x;
+                        right_rotate(t, z);
+                        x = z->parent;
+                    }
+                    x->rb = BLACK;
+                    x->parent->rb = RED;
+                    left_rotate(t, x->parent);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+static void rb_delete_fixup(bst_t* t, struct bst_node* x){
+    struct bst_node* w;
+    
+    if(x->rb == RED || x->parent == t->nil){
+        x->rb = BLACK;
+    } else if (x == x->parent->left){
+        w = x->parent->right;
+        if(w->rb == RED){
+            w->rb = BLACK;
+            x->parent->rb = RED;
+            left_rotate(t, x->parent);
+            w = x->parent->right;
+        }
+        if(w->left->rb == BLACK && w->right->rb == BLACK){
+            w->rb = RED;
+            rb_delete_fixup(t, x->parent);
+        }
+        else {
+            if(w->right->rb == BLACK){
+                w->left->rb = BLACK;
+                w->rb = RED;
+                right_rotate(t, w);
+                w = x->parent->right;
+            }
+            w->rb = x->parent->rb;
+            x->parent->rb = BLACK;
+            w->right->rb = BLACK;
+            left_rotate(t, x->parent);
+        }
+    } else {
+        w = x->parent->left;
+        if(w->rb == RED){
+            w->rb = BLACK;
+            x->parent->rb = RED;
+            right_rotate(t, x->parent);
+            w = x->parent->left;
+        }
+        if(w->right->rb == BLACK && w->left->rb == BLACK){
+            w->rb = RED;
+            rb_delete_fixup(t, x->parent);
+        }
+        else {
+            if(w->left->rb == BLACK){
+                w->right->rb = BLACK;
+                w->rb = RED;
+                left_rotate(t, w);
+                w = x->parent->left;
+            }
+            w->rb = x->parent->rb;
+            x->parent->rb = BLACK;
+            w->left->rb = BLACK;
+            right_rotate(t, x->parent);
+        }
+    }
+}
+
 
 static struct bst_node* tree_insert(bst_t* t, int n){
     struct bst_node* x;
@@ -87,10 +242,10 @@ static struct bst_node* tree_insert(bst_t* t, int n){
     z->steps = INFTY;
     z->next = NULL;
 
-    y = NULL;
+    y = t->nil;
     x = t->root;
 
-    while(x!=NULL){
+    while(x!=t->nil){
         y = x;
         if(z->key < x->key){
             x = x->left;
@@ -103,7 +258,7 @@ static struct bst_node* tree_insert(bst_t* t, int n){
     }
     z->parent = y;
 
-    if(y == NULL){
+    if(y == t->nil){
         t->root = z;
     } else if(z->key < y->key){
         y->left = z;
@@ -114,10 +269,13 @@ static struct bst_node* tree_insert(bst_t* t, int n){
         y->right = z;
     }
     
-    z->left = NULL;
-    z->right = NULL;
+    z->left = t->nil;
+    z->right = t->nil;
+    z->rb = RED;
 
     (t->size)++;
+
+    rb_insert_fixup(t, z);
        
     return z;
 }
@@ -126,21 +284,21 @@ static void tree_delete(bst_t* t, struct bst_node* z){
     struct bst_node* y;
     struct bst_node* x;
 
-    if(z->left == NULL || z->right == NULL)
+    if(z->left == t->nil || z->right == t->nil)
         y = z;
     else
-        y = tree_successor(z);
+        y = tree_successor(t, z);
 
-    if(y->left != NULL){
+    if(y->left != t->nil){
         x = y->left;
     } else {
         x = y->right;
     }
    
-    if(x != NULL)
-        x->parent = y->parent;
+    // if(x != t->nil)
+    x->parent = y->parent;
     
-    if(y->parent == NULL){
+    if(y->parent == t->nil){
         t->root = x;
     } else if(y == y->parent->left){
         y->parent->left = x;
@@ -153,21 +311,29 @@ static void tree_delete(bst_t* t, struct bst_node* z){
         memcpy(&(z->cars), &(y->cars), sizeof(struct heap));
     }
 
+    if(y->rb == BLACK){
+        rb_delete_fixup(t, x);
+    }
+
     free(y);
 }
 
-static struct bst_node* tree_search_util(struct bst_node* x, int n){
-    if(x == NULL || x->key == n)
+static struct bst_node* tree_search_util(struct bst_node* x, int n, struct bst_node* nil){
+    if(x == nil || x->key == n){
         return x;
-    if(n < x->key){ 
-        return tree_search_util(x->left, n);
-    } else {
-        return tree_search_util(x->right, n);
     }
+
+    // if(n < x->key){ 
+    //     return tree_search_util(x->left, n, nil);
+    // } else {
+    //     return tree_search_util(x->right, n, nil);
+    // }
+
+    return tree_search_util((struct bst_node*)((unsigned long int)(x->right) * (n >= x->key) + (unsigned long int)(x->left) * (n < x->key)), n, nil);
 }
 
 static struct bst_node* tree_search(bst_t* t, int n){
-    return tree_search_util(t->root, n);
+    return tree_search_util(t->root, n, t->nil);
 }
 
 // static void tree_destroy_util(struct bst_node* x){
@@ -189,7 +355,8 @@ static void max_heapify(struct heap* h, int i){
     int max = NIL;
     int tmp = NIL;
 
-    (l <= h->size && h->a[l] > h->a[i]) ? (max = l) : (max = i);
+    // (l <= h->size && h->a[l] > h->a[i]) ? (max = l) : (max = i);
+    max = (l <= h->size && h->a[l] > h->a[i]) * l + !(l <= h->size && h->a[l] > h->a[i]) * i;
     if(r <= h->size && h->a[r] > h->a[max])
         max = r;
     if(max != i){
@@ -237,10 +404,11 @@ static void enqueue(queue_t* queue, struct bst_node* x){
     }
 
     queue->q[queue->tail] = x;
-    if(queue->tail == queue->length)
-        queue->tail = 0;
-    else
-        queue->tail = queue->tail + 1;
+    // if(queue->tail == queue->length)
+    //     queue->tail = 0;
+    // else
+    //     queue->tail = queue->tail + 1;
+    queue->tail = (queue->tail != queue->length) * queue->tail + 1;
 }
 
 static struct bst_node* dequeue(queue_t* queue){
@@ -251,10 +419,11 @@ static struct bst_node* dequeue(queue_t* queue){
     
     x = queue->q[queue->head];
     queue->q[queue->head] = NULL;
-    if(queue->head == queue->length)
-        queue->head = 0;
-    else
-        queue->head = queue->head + 1;
+    // if(queue->head == queue->length)
+    //     queue->head = 0;
+    // else
+    //     queue->head = queue->head + 1;
+    queue->head = (queue->head != queue->length) * queue->head + 1;
     return x;
 }
 
@@ -285,7 +454,7 @@ static void aggiungi_stazione(bst_t* autostrada, int dist, int num){
 static void aggiungi_auto(bst_t* autostrada, int stazione, int autonomia){
     struct bst_node* s;
     s = tree_search(autostrada, stazione);
-    if(s == NULL){
+    if(s == autostrada->nil){
         fprintf(stdout, "non aggiunta\n");
         return;
     }
@@ -299,7 +468,7 @@ static void aggiungi_auto(bst_t* autostrada, int stazione, int autonomia){
 static void demolisci_stazione(bst_t* autostrada, int stazione){
     struct bst_node* s;
     s = tree_search(autostrada, stazione);
-    if(s == NULL){
+    if(s == autostrada->nil){
         fprintf(stdout, "non demolita\n");
         return;
     }
@@ -313,7 +482,7 @@ static void rottama_auto(bst_t* autostrada, int stazione, int autonomia){
     struct bst_node* s;
 
     s = tree_search(autostrada, stazione);
-    if(s == NULL){
+    if(s == autostrada->nil){
         fprintf(stdout, "non rottamata\n");
         return;
     }
@@ -326,33 +495,32 @@ static void rottama_auto(bst_t* autostrada, int stazione, int autonomia){
     fprintf(stdout, "rottamata\n");
 }
 
-static void reset(struct bst_node* x){
-    if(x != NULL){
+static void reset(struct bst_node* x, struct bst_node* nil){
+    if(x != nil){
         x->color = WHITE;
         x->steps = INFTY;
         x->next = NULL;
-        reset(x->right);
-        reset(x->left);
+        reset(x->right, nil);
+        reset(x->left, nil);
     }
 }
 
-static void find_adjacents(struct bst_node* x, struct bst_node* origin, queue_t* q, int dir){
-    if(x != origin && (x->key > origin->key) == dir && get_max_car(origin) >= abs(x->key - origin->key)){
-        if((unsigned int)x->steps > (unsigned int)(origin->steps)){
-            x->steps = origin->steps + 1;
-            if(x->color == WHITE){
-                enqueue(q, x);
-            }
-            if(x->next==NULL || (x->next!=NULL && origin->key < x->next->key)){
-                x->next = origin;
-            }
-            x->color = GRAY;
+static void find_adjacents(struct bst_node* x, struct bst_node* origin, queue_t* q, int dir, struct bst_node* nil){
+    if((x != origin && (x->key > origin->key) == dir && get_max_car(origin) >= abs(x->key - origin->key)) && ((unsigned int)x->steps > (unsigned int)(origin->steps))){
+        x->steps = origin->steps + 1;
+        if(x->color == WHITE){
+            enqueue(q, x);
         }
+        if(x->next==NULL || (x->next!=NULL && origin->key < x->next->key)){
+            x->next = origin;
+        }
+        // x->next = (struct bst_node*)((unsigned long int)origin * (x->next==NULL || (x->next!=NULL && origin->key < x->next->key)) + (unsigned long int)x->next * !(x->next==NULL || (x->next!=NULL && origin->key < x->next->key)));
+        x->color = GRAY;
     }
-    if(x->left!=NULL && (dir || (!dir && get_max_car(origin) > origin->key - x->key)) && (!dir || (dir && origin->key < x->key))) 
-        find_adjacents(x->left, origin, q, dir);
-    if(x->right!=NULL && (!dir || (dir && get_max_car(origin) > x->key - origin->key)) && (dir || (!dir && origin->key > x->key)))
-        find_adjacents(x->right, origin, q, dir);
+    if(x->left!=nil && (dir || (!dir && get_max_car(origin) > origin->key - x->key)) && (!dir || (dir && origin->key < x->key))) 
+        find_adjacents(x->left, origin, q, dir, nil);
+    if(x->right!=nil && (!dir || (dir && get_max_car(origin) > x->key - origin->key)) && (dir || (!dir && origin->key > x->key)))
+        find_adjacents(x->right, origin, q, dir, nil);
 }
 
 static void print_path(struct bst_node* x){
@@ -365,10 +533,11 @@ static void print_path(struct bst_node* x){
 static int find_best_path(bst_t* t, struct bst_node* from, struct bst_node* to, queue_t* q){
     struct bst_node* u;
     int break_loop = 0;
+    int dir;
     
     u = NULL;
 
-    reset(t->root);
+    reset(t->root, t->nil);
 
     from->color = GRAY;
     from->steps = 0;
@@ -391,7 +560,15 @@ static int find_best_path(bst_t* t, struct bst_node* from, struct bst_node* to, 
             if(!u->cars.max_heapified){
                 build_max_heap(&(u->cars));
             }
-            find_adjacents(t->root, u, q, to->key > from->key);
+            dir = to->key > from->key;
+            // if(t->root->key < u->key && dir){
+            //     find_adjacents(t->root->right, u, q, dir, t->nil);
+            // } else if (t->root->key > u->key && !dir){
+            //     find_adjacents(t->root->left, u, q, dir, t->nil);
+            // } else {
+            //     find_adjacents(t->root, u, q, dir, t->nil);
+            // }
+            find_adjacents(t->root, u, q, dir, t->nil);
         }
         u->color = BLACK;
     }
@@ -468,35 +645,12 @@ static void parse_and_execute(bst_t* autostrada){
 
 }
 
-// static void debug_tree_util(bst_t* t, struct bst_node* node, int space){
-//     if(node == NULL) return;
-// 
-//     space += 5;
-// 
-//     debug_tree_util(t, node->right, space);
-// 
-//     fprintf(stderr, "\n");
-//     for(int i=5; i<space; i++)
-//         fprintf(stderr, " ");
-//     fprintf(stderr, node->color == WHITE ? "%d\n" : "\033[31m%d\033[0m\n", node->key);
-// 
-//     debug_tree_util(t, node->left, space);
-// }
-// 
-// static void debug_tree(bst_t* t){
-//     fprintf(stderr, "size: %lu\n", t->size);
-//     debug_tree_util(t, t->root, 0);
-//     fprintf(stderr, "-----------------------------------------------------------------------\n");
-// }
-
 int main(int argc, char** argv) {
     bst_t* autostrada;
 
     autostrada = init_tree();
 
     parse_and_execute(autostrada);
-    
-    // debug_tree(autostrada);
     
     // tree_destroy(autostrada);
     
